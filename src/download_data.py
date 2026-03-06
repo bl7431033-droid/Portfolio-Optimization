ffa4934d49a25ff9442008e664a3c865e5f716a8
Brian Lin <bl7431033@gmail.com>
2026-03-05 21:41:38 -0500

    Download stock data and save to CSV

--- src/download_data.py
+++ src/download_data.py
@@ -1,2 +1,8 @@
-yf.download()
-data.to_csv()
+import yfinance as yf
+import pandas as pd
+
+stocks = ["AAPL","MSFT","NVDA","SPY"]
+
+data = yf.download(stocks, start="2018-01-01")["Close"]
+
+data.to_csv("prices.csv")
